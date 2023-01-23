53b0f29c43c69717536a7b5ed872e0355ab20fe4
Developer Ashish <therobomarket@gmail.com>
2023-01-23 12:32:48 +0530

    train and evaluate function completed; ready for training

--- train.py
+++ train.py
@@ -11,6 +11,49 @@ from data import RetinaDataset
 
 
 
+def train(model, loader, optimizer, loss_fn, device):
+    epoch_loss = 0.0
+    
+    model.train()    # Training Mode ON
+    for x, y in loader:
+        x = x.to(device, dtype = torch.float32)
+        y = y.to(device, dtype = torch.float32)
+        
+        optimizer.zero_grad()
+        y_pred = model(x)
+        loss = loss_fn(y_pred, y)
+        loss.backward()
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+    
+    epoch_loss = epoch_loss / len(loader)
+    
+    return epoch_loss
+
+
+
+def evaluate(model, loader, loss_fn, device):
+    epoch_loss = 0.0
+    
+    model.eval()    # Eval Mode ON
+    with torch.inference_mode():
+        for x, y in loader:
+            x = x.to(device, dtype = torch.float32)
+            y = y.to(device, dtype = torch.float32)
+            
+            y_pred = model(x)
+            loss = loss_fn(y_pred, y)
+            epoch_loss += loss.item()
+        
+        epoch_loss = epoch_loss / len(loader)
+        
+    return epoch_loss    
+
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -36,7 +79,7 @@ if __name__ == "__main__":
     W = 512
     SIZE = (H, W)
     BATCH_SIZE = 2
-    NUM_EPOCHS = 50
+    NUM_EPOCHS = 5
     LR = 0.001
     CHECKPOINT_PATH = os.path.join(os.getcwd(), "files", "checkpoint.pth")
     
@@ -74,4 +117,24 @@ if __name__ == "__main__":
     loss_fn = DiceBCELoss()
     
     
-    
+    """ Training the Model """
+    for epoch in range(0, NUM_EPOCHS):
+        start_time = time.time()
+        
+        train_loss = train(model = model, loader = train_loader, 
+                           optimizer = optimizer, loss_fn = loss_fn, 
+                           device = device)
+        
+        valid_loss = evaluate(model = model, loader = valid_loader, 
+                              loss_fn = loss_fn, device = device)
+        
+        end_time = time.time()
+        
+        epoch_mins, epoch_secs = epoch_time(start_time = start_time, 
+                                            end_time = end_time)
+        
+        
+        data_str = f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s\n'
+        data_str += f'\tTrain Loss: {train_loss:.3f}\n'
+        data_str += f'\t Val. Loss: {valid_loss:.3f}\n'
+        print(data_str)
